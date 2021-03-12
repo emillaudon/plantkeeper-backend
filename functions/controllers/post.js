861cf91a774156f1dbca7e6079e3fa7a405a54bf
emillaudon <emil.laudn@gmail.com>
2021-03-12 17:38:43 +0100

    first commit

---
+++ functions/controllers/post.js
@@ -0,0 +1,63 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+const admin = require('firebase-admin');
+const db = admin.firestore();
+
+const authMiddleWare = require('../authMiddleware');
+
+const postApp = express();
+postApp.use(authMiddleWare);
+
+postApp.use(cors({ origin: true }));
+
+postApp.get('/:id', async (req, res) => {
+    const userId = req.params.id;
+    const snapshot = await db.collection('users').doc(id).collection('posts').get();
+
+    let posts = [];
+    snapshot.forEach(doc => {
+        let id = doc.id;
+        let data = doc.data();
+
+        posts.push({ id, ...data });
+
+        res.status(200).send(JSON.stringify(posts));
+    });
+
+});
+
+postApp.post('/new/:id', async (req, res) => {
+    const userId = req.params.id;
+    const title = req.body.title;
+
+    const data = {
+        title: title,
+        f: title
+    }
+
+    await db.collection('users').doc(userId).collection('posts').add(data);
+
+    res.status(201).send();
+});
+
+postApp.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = req.params.body;
+
+    await db.collection('users').doc(userId).collection('posts').doc(id).update(body);
+
+    res.status(200).send();
+});
+
+postApp.delete('/:id', async (req, res) => {
+    const userId = req.params.id;
+    const docId = JSON.parse(req.body.postId);
+
+    await db.collection('users').doc(userId).collection('posts').doc(docId).delete();
+
+    res.status(200).send();
+});
+
+exports.post = functions.https.onRequest(postApp);
---
+++ functions/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    ...require('./controllers/user'),
+    ...require('./controllers/post')
+}
